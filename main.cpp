87c970dbb9d8062be2de91da26ec58ac4dfb79c5
Retro <tzou.righteou@gmail.com>
2023-01-29 08:27:09 -0800

    few code changed

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018 Torsten Muetze
+ * Copyright (c) 2022 Retro Zhou
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
