3798145231498f07a0bd1886627e74393a0b723f
Retro <tzou.righteou@gmail.com>
2023-01-29 23:55:56 -0800

    changed a few code structure

--- main.cpp
+++ main.cpp
@@ -149,7 +149,8 @@ int main(int argc, char *argv[]) {
         } 
         // const ulong *cx = x.data();  // cx[1]*1 + cx[2]*2 + ... + cx[n]*n == n
         for (int k = 1; k <= n; k++) {
-          if (cx[k] == 0) continue;
+          if (cx[k] == 0) 
+            continue;
           for (int j = 1; j <= cx[k]; ++j) {
             std::cout << k << " ";
           }
